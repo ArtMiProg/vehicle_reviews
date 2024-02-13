6ef1e5470740fed55ba15d6cce58b36289829bbf
ArtMiProg <am.poshta1@gmail.com>
2024-02-13 11:15:49 +0300

    Search car preliminary

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,8 +1,9 @@
 import { KeyboardArrowDown } from "@mui/icons-material";
-import { IconButton, Typography } from "@mui/material";
+import { Card, CardActions, CardContent, IconButton, TextField, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import SearchIcon from '@mui/icons-material/Search';
 import { styled } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -25,13 +26,14 @@ const BannerContainer = styled(Box)(({ theme }) => ({
     backgroundRepeat: 'no-repeat',
     minHeight: '400px',
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'left',
     alignItems: 'center',
     backgroundSize: '100% 100%',
 }));
 
 const BannerContent = styled(Box)(({ theme }) => ({
     textAlign: 'center',
+    marginLeft: '150px',
     color: theme.palette.mode === 'dark' ? '#fff' : '#000',
 }));
 
@@ -76,6 +78,8 @@ function StartPage() {
     // const  allReviews : Review[] = useAppSelector((state : ReviewsState) => state.reviews.reviews);
 
     const [allCars, setAllCars] = useState<StrapiListResponse<StrapiCar>>();
+    const [searchInput, setSearchInput] = useState<string>("");
+
     useEffect(() => {
         const load = async () => {
             try {
@@ -91,6 +95,14 @@ function StartPage() {
         }
         load();
     }, []);
+
+    const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchInput(event.target.value);
+    };
+
+    const filteredCars = allCars?.data.filter(car =>
+        car.attributes.maker && car.attributes.maker.toLowerCase().includes(searchInput.toLowerCase())
+    );
 
     return (
         <>
@@ -112,26 +124,46 @@ function StartPage() {
                     <IconButton
                         sx={{
                             position: 'absolute',
-                            bottom: '140px', 
+                            bottom: '140px',
                             left: '50%',
                             transform: 'translateX(-50%)',
                             color: 'primary.main',
                         }}
                         onClick={() => {
                             window.scrollBy({
-                                top: window.innerHeight * 0.6, 
+                                top: window.innerHeight * 0.6,
                                 behavior: 'smooth',
                             });
                         }}
                     >
                         <KeyboardArrowDown />
                     </IconButton>
+                    <Card sx={{position: 'absolute', top: '100px', right: '30px', backgroundColor: '#E0E0E0', alignItems: 'center'}}>
+                        <CardContent>
+                            <TextField
+                                sx={{  backgroundColor: 'white', top: '6px' }}
+                                label="Search car"
+                                variant="outlined"
+                                value={searchInput}
+                                onChange={handleSearchInputChange}
+                            />
+                            <SearchIcon sx={{ fontSize: '55px', color: 'disabled' }} />
+                        </CardContent>
+                    </Card>
                 </BannerContent>
             </BannerContainer>
             <Box sx={{ flexGrow: 1 }}>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} justifyContent="center">
+                    {filteredCars ? filteredCars?.map((car) => (
+                        <Grid item xs={3.5}>
+                            <Item>
+                                <OneCar key={car.id} car={car} />
+                            </Item>
+                        </Grid>
+                    )) : <Typography>Loading...</Typography>
+                    }
                     {allCars ? allCars.data.map((car) => (
-                        
+
                         <Grid item xs={3.5}>
                             <Item>
                                 <OneCar key={car.id} car={car} />
