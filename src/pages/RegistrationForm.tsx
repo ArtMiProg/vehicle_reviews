1292c5175808d0aa41a76efbca7a451bcde99ac7
ArtMiProg <am.poshta1@gmail.com>
2024-02-14 10:50:07 +0300

    Alert

--- src/pages/RegistrationForm.tsx
+++ src/pages/RegistrationForm.tsx
@@ -53,6 +53,11 @@ function RegistrationForm() {
             return;
         }
 
+        if (password.length < 6) {
+            setErrorMessage("Password must be at least 6 characters long");
+            return;
+        }
+
         const updatedUsers = [...existingUsers, newUser];
         // localStorage.setItem("users", JSON.stringify(updatedUsers));
         try {
@@ -63,22 +68,22 @@ function RegistrationForm() {
                 provider: "local",
                 confirmed: false,
                 profile_slug: name,
-                role: {connect: [{ id: 1 }]},
+                role: { connect: [{ id: 1 }] },
                 name: name,
                 surname: surname
             }
             );
             setAlert({ show: true, severity: 'success', message: 'Registration successful' });
-        setTimeout(() => {
-            setAlert({ show: false, severity: undefined, message: '' });
-            window.location.href = '/';
-        }, 3000);
-        }catch (error){
+            setTimeout(() => {
+                setAlert({ show: false, severity: undefined, message: '' });
+                window.location.href = '/';
+            }, 3000);
+        } catch (error) {
             console.error('error creating user', error);
         }
-        
+
     };
-    
+
     const handleCancel = () => {
         window.history.back();
     }
@@ -86,74 +91,79 @@ function RegistrationForm() {
 
     return (
         <Container component="main" maxWidth="sm">
-        <Box
-            sx={{
-                marginTop: 8,
-                display: 'flex',
-                flexDirection: 'column',
-            }}
-        >
-            {alert.show && (
-                <Typography>
-                    <div>Redirecting in {countdown}...</div>
-                    <CustomAlert severity={alert.severity} message={alert.message} />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
+                {alert.show && (
+                    <Typography>
+                        <div>Redirecting in {countdown}...</div>
+                        <CustomAlert severity={alert.severity} message={alert.message} />
+                    </Typography>
+                )}
+                <Typography component="h1" variant="h5" style={{ margin: '20px 0' }}>
+                    Sign up
                 </Typography>
-            )}
-            <Typography component="h1" variant="h5" style={{ margin: '20px 0' }}>
-                Sign up
-            </Typography>
-            <form onSubmit={handleSubmit} style={{ width: '100%' }}>
-                <TextField
-                    label="Name"
-                    variant="outlined"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    margin="normal"
-                    fullWidth
-                />
-                <TextField
-                    label="Surname"
-                    variant="outlined"
-                    value={surname}
-                    onChange={(e) => setSurname(e.target.value)}
-                    margin="normal"
-                    fullWidth
-                />
-                <TextField
-                    label="Email"
-                    variant="outlined"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    margin="normal"
-                    fullWidth
-                />
-                <TextField
-                    label="Username"
-                    variant="outlined"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    margin="normal"
-                    fullWidth
-                />
-                <TextField
-                    label="Password"
-                    type="password"
-                    variant="outlined"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    margin="normal"
-                    fullWidth
-                />
-                <Button variant="contained" type="submit" color="primary">
-                    Register
-                </Button>
-                <Button variant="contained" color="secondary" style={{ marginLeft: '10px' }} onClick={handleCancel}>
-                    Cancel
-                </Button>
-            </form>
-        </Box>
-    </Container>
-);
+                <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+                    <TextField
+                        label="Name"
+                        variant="outlined"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        margin="normal"
+                        fullWidth
+                    />
+                    <TextField
+                        label="Surname"
+                        variant="outlined"
+                        value={surname}
+                        onChange={(e) => setSurname(e.target.value)}
+                        margin="normal"
+                        fullWidth
+                    />
+                    <TextField
+                        label="Email"
+                        variant="outlined"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        margin="normal"
+                        fullWidth
+                    />
+                    <TextField
+                        label="Username"
+                        variant="outlined"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        margin="normal"
+                        fullWidth
+                    />
+                    <TextField
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        margin="normal"
+                        fullWidth
+                    />
+                    {errorMessage && (
+                        <Typography variant="body2" color="error" style={{ marginBottom: '10px' }}>
+                            {errorMessage}
+                        </Typography>
+                    )}
+                    <Button variant="contained" type="submit" color="primary">
+                        Register
+                    </Button>
+                    <Button variant="contained" color="secondary" style={{ marginLeft: '10px' }} onClick={handleCancel}>
+                        Cancel
+                    </Button>
+                </form>
+            </Box>
+        </Container>
+    );
 }
 
 export default RegistrationForm;
